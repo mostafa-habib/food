4d062519a6dbacd6e5e57667fc10a727761c6ee1
Mostafa Habib <sasahabeb86@gmail.com>
2024-09-08 09:56:14 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,10 @@
 import mongoose from "mongoose";
-import mongoose from "mongoose";
 require('dotenv').config();
 const mongoUrl = process.env.MONGODB_URI;
 
 
 export const connectDB = async () => {
     await mongoose.connect(mongoUrl).then(() => {
-        console.log("DB Connected")
-    })
-}
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://mostafahabib866:Fatma_1700557@cluster0.bet8k.mongodb.net/food').then(() => {
         console.log("DB Connected")
     })
 }
